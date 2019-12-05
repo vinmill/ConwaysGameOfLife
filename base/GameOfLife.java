e455099880f9d3e20f01e3a9703be6c3b1b95821
Calvin Milligan <cmilligan@cmilligan1.lan>
2019-12-04 20:32:55 -0500

    Start double linked list

--- base/GameOfLife.java
+++ base/GameOfLife.java
@@ -10,12 +10,12 @@ public class GameOfLife {
 	private static float p = 0.20f;
 	public static void main(String[] args) {
 		GameOfLife newGame = new GameOfLife();
-		char[][] newMatrix = new char[rows][columns];
+		int[][] newMatrix = new int[rows][columns];
 		newGame.startSim(newMatrix,rows,columns);
 	}
 
 	// Start the game
-	public void startSim(char[][] matrix,int rows, int columns) {
+	public void startSim(int[][] matrix,int rows, int columns) {
 		Scanner s = new Scanner(System.in);
 		setMatrix(matrix, p);
 		getMatrix(matrix);
@@ -38,9 +38,9 @@ public class GameOfLife {
 	
 
 	// Create a method to generate a new matrix
-	public char[][] lifeCyle(char[][] matrix, int rows, int columns) {
+	public int[][] lifeCyle(int[][] matrix, int rows, int columns) {
  
-		char[][] m = new char[rows][columns];
+		int[][] m = new int[rows][columns];
 
 		for(int i = 0; i < rows; i++){
 			for(int j = 0; j < columns; j++){
@@ -52,7 +52,7 @@ public class GameOfLife {
 
 
 	//Count the number of living cell neighboring a cell at the given coordinate
-	public int around(char[][] matrix, int i, int j) {
+	public int around(int[][] matrix, int i, int j) {
 		int count = 0, x, y;
 		for(x = i - 1; x <= i + 1; x++){
 			for(y = j - 1; y <= j + 1; y++)
@@ -69,55 +69,55 @@ public class GameOfLife {
 	}
 
 	//Create a godMode that will take a matrix and position variables and will return either a dead or alive cell
-	public char godMode(char[][] matrix, int i, int j) {
+	public int godMode(int[][] matrix, int i, int j) {
 		int count = around(matrix, i, j);
 
-		if (matrix[i][j] == '1'){
+		if (matrix[i][j] == 1){
 			if (count < 2 || count > 3) {
-				return '0';
+				return 0;
 			}
-			return '1';
+			return 1;
 		}
 		else {
 			if (count == 3) {
-				return '1';
+				return 1;
 			}
-			return '0';
+			return 0;
 		}
 	}
 
 	//Evaluate the matrix with 
-	public int eval(char[][] matrix, int i, int j) {
+	public int eval(int[][] matrix, int i, int j) {
 		int val = 0;
 		if(i < 0 || j < 0 || i == rows || j == columns) {
 			return val = 0;
 		}
 
-		if (matrix[i][j] == '1') {
+		if (matrix[i][j] == 1) {
 			return val = 1;
 		}
 		return val;
 	}
 
 	//Initialize an array
-	public void setMatrix(char[][] matrix, float percentageFill) {
+	public void setMatrix(int[][] matrix, float percentageFill) {
 		for(int i = 0; i < matrix.length;i++){
-			char [] array = matrix[i];
+			int[] array = matrix[i];
 			for(int j = 0; j < array.length; j++) {
 				if (Math.random() < percentageFill) {
-					array[j] = '1';
+					array[j] = 1;
 				}
 				else {
-					array[j] = '0';
+					array[j] = 0;
 				}
 			}
 		}
 	}
 
 	//Print the array
-	public void getMatrix(char[][] matrix) {
+	public void getMatrix(int[][] matrix) {
 		for(int i = 0; i < matrix.length; i++){
-			char[] array = matrix[i];
+			int[] array = matrix[i];
 			for(int j = 0; j < array.length; j++){
 				System.out.print(array[j]);
 			}
--- base/Node.java
+++ base/Node.java
@@ -1,31 +1,49 @@
-class Node {
-    char[][] payload;
-    Node next;
-  
-    public Node(){
-      payload = ;
-      next = null;
-    } // end constructor
-  
-    public Node(char[][] payload, Node next){
-      this.payload = payload;
-      this.next = next;
-    } // end constructor
-  
-    public char[][] getPayload(){
-      return this.payload;
-    } // end getPayload
-  
-    public void setPayload(char[][] payload){
-      this.payload = payload;
-    } // end setPayload
-  
-    public Node getNext(){
-      return this.next;
-    } // end getNext
-  
-    public void setNext(Node next){
-      this.next = next;
-    } // end setNext
-  
-  } // end Node class
+/** Node2.java
+    node for a double-linked list
+*/
+
+public class Node2 {
+  protected String payload;
+  protected Node2 previous;
+  protected Node2 next;
+
+  public Node2(){
+    this.payload = "";
+    this.previous = null;
+    this.next = null;
+  } // end constructor
+
+  public Node2(String payload, Node2 previous, Node2 next){
+    this.payload = payload;
+    this.previous = previous;
+    this.next = next;
+  } // end constructor
+
+  // payload accessors
+  public String getPayload(){
+    return this.payload;
+  } // end getPayload
+
+  public void setPayload(String payload){
+    this.payload = payload;
+  } // end setPayload
+
+  // previous accessors
+  public Node2 getPrevious(){
+    return this.previous;
+  } // end getPrevious
+
+  public void setPrevious(Node2 previous){
+    this.previous = previous;
+  } // end setPrevious
+
+  // next accessors
+  public Node2 getNext(){
+    return this.next;
+  } // end getNext
+
+  public void setNext(Node2 next){
+    this.next = next;
+  } // end setNext
+
+} // end class def
